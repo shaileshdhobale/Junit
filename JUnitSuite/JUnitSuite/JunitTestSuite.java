1e7303680e39c7e0fb93ac0dcee4ec5a0b1d690d
shaileshdhobale <shailesh.dhobale@mindstix.com>
2017-01-06 14:52:24 +0530

    Added Suite test example.

---
+++ JUnitSuite/JUnitSuite/JunitTestSuite.java
@@ -0,0 +1,10 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   JunitTest1.class,
+   JunitTest2.class
+})
+public class JunitTestSuite {   
+}  	
---
+++ JunitSuiteTest/src/JunitTestSuite.java
@@ -0,0 +1,12 @@
+package src;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   JunitTest1.class,
+   JunitTest2.class
+})
+public class JunitTestSuite {   
+}  	
